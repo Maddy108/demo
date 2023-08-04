62970172e8f7cb3e0c2ccdcd2dc97585d590e3c2
Maddy108 <141391423+Maddy108@users.noreply.github.com>
2023-08-03 18:43:34 -1200

    Update  by dev2 Maddy.java

--- Maddy.java
+++ Maddy.java
@@ -1,5 +1,6 @@
 public class Maddy{
     public static void main(String []args ){
         System.out.println("hello i am new Dev Maddy");
+         System.out.println("hello i am new Dev2 Maddy");
     }
 }
