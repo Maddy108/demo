28e8d3e5335a3cdc3aeab432303de27b95112f93
Maddy108 <141391423+Maddy108@users.noreply.github.com>
2023-08-03 18:17:05 -1200

    Update by dev 2 Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main (String arg[]){
         System.out.println("This is the Git Program by dev1");
+         System.out.println("This is the Git Program by dev2");
     }
 }
