ce278752b376f5a3d413268ed057718ef03bd5b3
Maddy108 <141391423+Maddy108@users.noreply.github.com>
2023-08-03 18:32:26 -1200

    Update Dev 2 Buddy.java

--- Buddy.java
+++ Buddy.java
@@ -1,5 +1,6 @@
 public class Buddy{
     public static void main(String []args ){
         System.out.println("hello i am new Dev Buddy");
+         System.out.println("hello i am new Dev2 Buddy");
     }
 }
